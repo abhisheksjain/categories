958393bdc3ff635c6f6a87efa388e57d5f2a4837
Abhishek Jain <abhishek.sjain@gmail.com>
2017-07-14 17:45:41 -0700

    Changes to be committed: 	modified:   pom.xml modified:   src/main/java/com/abhi/categories/App.java 	modified:   src/test/java/com/abhi/categories/AppTest.java 	new file:   src/test/resources/testfiles/file1.txt 	new file:   src/test/resources/testfiles/file2.txt 	new file:   src/test/resources/testfiles/file3.txt new file:   src/test/resources/testfiles/file4.txt

--- src/main/java/com/abhi/categories/App.java
+++ src/main/java/com/abhi/categories/App.java
@@ -2,72 +2,138 @@ package com.abhi.categories;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.SortedMap;
-import java.util.TreeMap;
 
 /**
- * Hello world!
+ * Read a file (name passed in as 1st argument to program) -- Input
+ * Each line of the file has a category, a space, and a sub-category (sub-category can have whitespace within it)
+ * Want to:
+	-- Only process the pair (category, sub-category) once
+	-- If a pair appears twice, ignore the second one
+	-- Keep track of the order of the first occurrence of each pair
+	-- Keep track of the count for each category
+	-- Legal category values are: PERSON PLACE ANIMAL COMPUTER OTHER -- CONSTANT
+	-- Illegal category values should be ignored
  *
  */
 public class App 
 {
-	private static Set<String> set = null;
-	private static SortedMap<String, Integer> sortedMap = new TreeMap<String, Integer>();
 
+	//final List of pre-populated unique categories and arraylist that maintains insertion order. 
+	private final ArrayList<String> legalCategoriesList;
 	
-	static{
-		sortedMap.put("Cat1", 0);
-		sortedMap.put("Cat2", 0);
-		sortedMap.put("Cat3", 0);
-		set = sortedMap.keySet();
-	}
+	public static final String COUNTS_ARR = "COUNTS_ARR";
+	public static final String UNIQUE_CAT_LINE_SET = "UNIQUE_CAT_LINE_SET";
 	
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	public App(ArrayList<String> legalCategoriesList){
+		this.legalCategoriesList = legalCategoriesList;
+	}	
+	
+	/**
+	 * run main pass input file argument as path of the file /categories/file4.txt
+	 * @param args
+	 */
+    public static void main( String[] args ) {
+    	//List of pre-populated unique categories and arraylist that maintains insertion order. 
+    	ArrayList<String> legalCategoriesList = new ArrayList<String>();
+		legalCategoriesList.add("PERSON");
+		legalCategoriesList.add("PLACE");
+		legalCategoriesList.add("ANIMAL");
+		legalCategoriesList.add("COMPUTER");
+		legalCategoriesList.add("OTHER");
+		
+    	App app = new App(legalCategoriesList);
+    	if(args[0] != null){
+    		app.outputWriter(app.populateCategories(new File(args[0])));
+    	}
     }
+    
     /**
      * 
      * @param file
      */
-    public static void populateCategories(String file){
+    public Map<String, Object> populateCategories(File file){
     	Scanner scanner = null;
+    	Map<String, Object> resultMap = null;
     	try{ 
-    		scanner = new Scanner(new File(file));
+    		//Output count of each legal categories
+    		//pre-initialized array of category counts will use legalCategoriesList index as an order and increase counter.
+    		int[] counts = new int[legalCategoriesList.size()];
+    		
+    		//output : LinkedHashSet that keeps track of insertion order and set that store only unique Strings.
+    		Set<String> uniqueCategofiesLine = new LinkedHashSet<String>();
+    		
+    		
+    		//initializing file Scanner 
+    		scanner = new Scanner(file);
 	    	while (scanner.hasNextLine()) {
-	        	String category = parseCategory(scanner.nextLine());
-	        	if(category != null){
-	        		sortedMap.put(category, (sortedMap.get(category)+1));
+	    		String line = scanner.nextLine();
+	    		//sending line to parse from legalCategoriesList and getting back legal category if present otherwise null
+	        	String category = parseCategory(line);
+	        	if(category != null && uniqueCategofiesLine.add(line)){//ignore in case of null or this line was already considered in earlier run
+	        		int indexOf = this.legalCategoriesList.indexOf(category);
+	        		if(indexOf != -1){
+	        			counts[indexOf] = counts[indexOf]+1;//increase counter if found category
+	        		}
 	        	}
 			}
+	    	resultMap = new HashMap<String, Object>();
+	    	resultMap.put(COUNTS_ARR, counts);
+	    	resultMap.put(UNIQUE_CAT_LINE_SET, uniqueCategofiesLine);
     	}catch(FileNotFoundException e){
-    		
+    		System.err.println("Supplied file is not found, please check name or provide correct path.");
     	}finally {
+    		//close scanner to avoid resource leak.
 			if(scanner != null){
 				scanner.close();
 			}
 		}
+    	return resultMap;
     }
     /**
-     * utility method that parses String line and returns if already defined category in the static set exists.
+     * method that parses String line and returns if already defined category in the static set exists.
      * if determined category does not exist in the line, returns null.
      * it determine category based on first occurrence of space in the line.
      * @param String new line
+     * @param List<String> legalCategoriesList
      * @return String category
      */
-    public static String parseCategory(String line){
+    public String parseCategory(String line){
     	if(line != null && !line.isEmpty()){
     		int spaceIndex = line.indexOf(" ");
     		if(spaceIndex != -1){
     			String category = line.substring(0, spaceIndex);
-    			sortedMap.get(category);
-    			if(set.contains(category)){
+    			if(this.legalCategoriesList.contains(category)){//ignore category that is not found in legalCategoriesList
     				return category;
     			}
     		}
-    	}    	
+    	}   
+    	//case returning null if not worked with the business logic. 
     	return null;    	
+    }
+    
+    /**
+     * writing output to console 
+     */
+    @SuppressWarnings("unchecked")
+	public void outputWriter(Map<String, Object> resultMap){
+    	//Writing output 
+    	System.out.println("Category\tCount");
+    	int[] counts = (int[]) resultMap.get(COUNTS_ARR);
+        for(String string:this.legalCategoriesList){
+        	System.out.print(string+"\t");
+        	int indexOf = this.legalCategoriesList.indexOf(string);
+   			System.out.print(counts[indexOf]);
+    		System.out.print("\n");
+        }
+        System.out.print("\n");
+        Set<String> set = (Set<String>) resultMap.get(UNIQUE_CAT_LINE_SET);
+        set.stream().forEach(line -> System.out.println(line));
+      
     }
 }
--- src/test/java/com/abhi/categories/AppTest.java
+++ src/test/java/com/abhi/categories/AppTest.java
@@ -1,38 +1,88 @@
 package com.abhi.categories;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest    
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+	
+	private App app;
+	
+	@Before
+	public void init(){
+    	//List of pre-populated unique categories and arraylist that maintains insertion order. 
+    	ArrayList<String> legalCategoriesList = new ArrayList<String>();
+		legalCategoriesList.add("PERSON");
+		legalCategoriesList.add("PLACE");
+		legalCategoriesList.add("ANIMAL");
+		legalCategoriesList.add("COMPUTER");
+		legalCategoriesList.add("OTHER");
+		this.app = new App(legalCategoriesList);
+	}
+
+
+	@Test
+    public void testParseCategoryPositive()
     {
-        super( testName );
+		assertEquals("PLACE", this.app.parseCategory("PLACE San Francisco"));
+		
+		assertEquals("PERSON", this.app.parseCategory("PERSON Abhishek"));
+		assertEquals("PERSON", this.app.parseCategory("PERSON Abhishek Jain "));
+		
+		assertEquals("PLACE", this.app.parseCategory("PLACE New Delhi and "));
+		
+		//not clear about this test case, is empty or null sub category is valid or not assuming empty is valid sub category
+
+		assertEquals("OTHER", this.app.parseCategory("OTHER   "));
+    }
+	
+	@Test
+    public void testParseCategoryNegative()
+    {
+	
+		assertNull(this.app.parseCategory("CAT San Francisco"));
+		assertNull(this.app.parseCategory(" "));
+		assertNull(this.app.parseCategory("null"));
+		assertNull(this.app.parseCategory(" Abhi"));
+		assertNull(this.app.parseCategory(" PERSON"));
+    }
+	
+	@Test
+    public void testPopulateCategories()
+    {
+		int[] countsfile1 = {2, 2, 2, 1, 1};
+		Map<String, Object> map1 = this.app.populateCategories(new File(getClass().getClassLoader().getResource("testfiles/file1.txt").getFile()));	
+		assertArrayEquals(countsfile1, (int[]) map1.get(App.COUNTS_ARR));
+		
+		int[] countsfile2 = {2, 2, 2, 1, 1};
+		Map<String, Object> map2 = this.app.populateCategories(new File(getClass().getClassLoader().getResource("testfiles/file2.txt").getFile()));	
+		assertArrayEquals(countsfile2, (int[]) map2.get(App.COUNTS_ARR));
+		
+		int[] countsfile3 = {2, 2, 2, 1, 0};
+		Map<String, Object> map3 = this.app.populateCategories(new File(getClass().getClassLoader().getResource("testfiles/file3.txt").getFile()));	
+		assertArrayEquals(countsfile3, (int[]) map3.get(App.COUNTS_ARR));
+	
+    }
+	
+	@Test
+    public void testOutputWriter()
+    {
+		this.app.outputWriter(this.app.populateCategories(new File(getClass().getClassLoader().getResource("testfiles/file1.txt").getFile())));	
+		this.app.outputWriter(this.app.populateCategories(new File(getClass().getClassLoader().getResource("testfiles/file2.txt").getFile())));
+		this.app.outputWriter(this.app.populateCategories(new File(getClass().getClassLoader().getResource("testfiles/file3.txt").getFile())));	
+
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
